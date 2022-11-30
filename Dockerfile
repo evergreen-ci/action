0b386cfce35aada53ab6e1dfcbb3b5af7ad96e98
Brian Samek <brian.samek@mongodb.com>
2022-11-30 13:40:48 -0500

    try git config in shell script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM alpine:3.10
 RUN apk add git
-RUN git config --global user.email "brian.samek@mongodb.com"
-RUN git config --global user.name "evergreen-ci/action GitHub action"
 RUN wget -O /evergreen https://evergreen.mongodb.com/clients/linux_amd64/evergreen
 RUN chmod +x /evergreen
 COPY patch.sh /patch.sh
--- patch.sh
+++ patch.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 
 echo $EVERGREEN_AUTH | base64 -d > /.evergreen.yml
-echo "ls /"
-ls /
-echo "ls ."
-ls .
+git config --global user.email "brian.samek@mongodb.com"
+git config --global user.name "evergreen-ci/action GitHub action"
 /evergreen -c /.evergreen.yml patch -p evergreen-action -v all -t all -y -f
