2c0d6d31a34dcd9e02c8188ec28e7b3c690295cd
Brian Samek <brian.samek@mongodb.com>
2022-11-30 13:14:00 -0500

    try base64 secret

--- patch.sh
+++ patch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-echo $EVERGREEN_AUTH > /.evergreen.yml
+echo $EVERGREEN_AUTH | base64 -d > /.evergreen.yml
 /evergreen -c ~/.evergreen.yml patch -p evergreen-action -v all -t all -y -f
