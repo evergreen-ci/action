e14b346eed3e0751556c2895839838d16c78bf07
Brian Samek <brian.samek@mongodb.com>
2022-11-30 11:29:43 -0500

    submit a patch

--- patch.sh
+++ patch.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
 
 echo $EVERGREEN_AUTH > /.evergreen.yml
-/evergreen --help
-ls /.evergreen.yml
-wc /.evergreen.yml
-cat /.evergreen.yml
+/evergreen -c ~/.evergreen.yml patch -p evergreen-action -v all -t all -y -f
